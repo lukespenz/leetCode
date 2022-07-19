63955d848a0ebbea09367a512c6b2e8527c5a258
Lucas Spencer <lucas.spencer14@gmail.com>
2022-07-19 15:32:38 -0600

    js lc 347 updated solution

--- javascript/leetCode347.js
+++ javascript/leetCode347.js
@@ -1,33 +1,68 @@
-const topKFreq = (nums, k) => {
-    const hashMap = {}
+// const topKFreq = (nums, k) => {
+//     const hashMap = {}
 
-    for (let i = 0; i < nums.length; i++) {
-        if(nums[i] in hashMap){
-            strNum = nums[i].toString()
-            hashMap[`${strNum}`] += 1
+//     for (let i = 0; i < nums.length; i++) {
+//         if(nums[i] in hashMap){
+//             strNum = nums[i].toString()
+//             hashMap[`${strNum}`] += 1
+//         } else {
+//             strNum = nums[i].toString()
+//             hashMap[`${strNum}`] = 1
+//         }
+//     }
+//     arr = []
+//     max = []
+
+//     for (const [key, value] of Object.entries(hashMap)) {
+//         arr.push({key, value})
+//     }
+//     console.log(arr)
+
+//     arr = arr.sort((a,b) => b.value - a.value)
+
+//     for (let i = 0; i < k; i++) {
+//         console.log(arr[i])
+//         keyArr = Object.values(arr[i])
+//         max.push(parseInt(keyArr[0]))
+//     }
+//     return max
+// };
+
+// nums = [-1,-1,-1,2,2,3]
+// k = 2
+// console.log(topKFreq(nums, k))
+
+var topKFrequent = function(nums, k) {
+    // look up
+    // { element: # seen }
+    // {1: 3, 2: 2, 3: 1}
+    const seen = {}
+
+    for(let num of nums) {
+        if(seen[num] === undefined){
+            seen[num] = 1
         } else {
-            strNum = nums[i].toString()
-            hashMap[`${strNum}`] = 1
+            seen[num]++
         }
     }
-    arr = []
-    max = []
-
-    for (const [key, value] of Object.entries(hashMap)) {
-        arr.push({key, value})
+    // [[], [3], [2], [1]]
+    const bucket = []
+    for(let i = 0; i <= nums.length; i++){
+        bucket.push([])
     }
-    console.log(arr)
 
-    arr = arr.sort((a,b) => b.value - a.value)
-
-    for (let i = 0; i < k; i++) {
-        console.log(arr[i])
-        keyArr = Object.values(arr[i])
-        max.push(parseInt(keyArr[0]))
+    for(let key in seen){
+        let count = seen[key]
+        bucket[count].push(key)
     }
-    return max
-};
-
-nums = [-1,-1,-1,2,2,3]
-k = 2
-console.log(topKFreq(nums, k))
+    let res = [];
+    for(let i = bucket.length - 1; i>=0; i--){
+        if (bucket[i].length === 0){
+            continue
+        }else{
+            res = [...res, ...bucket[i]]
+            // [1,2,3]
+        }
+    }
+    return res.slice(0,k)
+}
